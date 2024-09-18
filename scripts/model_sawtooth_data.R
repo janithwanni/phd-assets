1b7c10e48ed9242b32d29406ea9c9325bc169d61
janithwanni <janithcwanni@gmail.com>
2024-09-18 23:06:49 +1000

    Move sawtooth models

--- scripts/model_sawtooth_data.R
+++ scripts/model_sawtooth_data.R
@@ -6,7 +6,7 @@ set.seed(345)
 train_data <- readr::read_csv(here::here("data/sawtooth_data/train_data.csv"))
 rfmodel <- randomForest(class ~ x + y, data = train_data |> mutate(class = factor(class)))
 rfmodel_bundled <- bundle(rfmodel)
-saveRDS(rfmodel_bundled, here::here("data/sawtooth_data/rfmodel.rds"))
+saveRDS(rfmodel_bundled, here::here("models/sawtooth_data/rfmodel.rds"))
 
 library(tidymodels)
 
@@ -16,7 +16,7 @@ xgbmodel <- boost_tree() |>
   fit(class ~ x + y, data = train_data |> mutate(class = factor(class)))
 
 xgb_bundled <- bundle(xgbmodel)
-saveRDS(xgb_bundled, here::here("data/sawtooth_data/xgbmodel.rds"))
+saveRDS(xgb_bundled, here::here("models/sawtooth_data/xgbmodel.rds"))
 
 train_data |> 
   mutate(
